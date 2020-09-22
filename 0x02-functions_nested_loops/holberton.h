86a635f67723abf9308c66d1abad80c94a396dfc
el-dani-cortes <2304@holbertonschool.com>
2020-09-22 11:44:39 +0000

    Header holberton.h with function prototypes

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,22 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*Putchar holberton function*/
+int _putchar(char c);
+
+/*Print alphabet lowercase*/
+void print_alphabet(void);
+
+/*Print alphabet 10 times*/
+void print_alphabet_x10(void);
+
+/*Checks for lowercase character*/
+int _islower(int c);
+
+/*Checks for alphabet character*/
+int _isalpha(int c);
+
+/*Give the sign of a number*/
+int print_sign(int n);
+
+#endif /* _HOLBERTON_H_ */
