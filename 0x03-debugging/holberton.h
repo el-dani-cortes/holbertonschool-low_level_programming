3db822ac2b9ca82d7a42a117ad2aeb18e715ec71
el-dani-cortes <2304@holbertonschool.com>
2020-09-23 20:44:18 +0000

    Add function that prints largest number and update holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,4 +6,7 @@
 /*Function that give the sign of any number*/
 void positive_or_negative(int i);
 
+/*Function that give the largest number of three*/
+int largest_number(int a, int b, int c);
+
 #endif /* HOLBERTON_H */
