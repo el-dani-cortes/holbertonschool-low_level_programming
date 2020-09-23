3d8852e09f71de9e369d5fe562eaded288e459bb
el-dani-cortes <2304@holbertonschool.com>
2020-09-23 23:12:32 +0000

    Add function to print remaining days and update holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -9,4 +9,10 @@ void positive_or_negative(int i);
 /*Function that give the largest number of three*/
 int largest_number(int a, int b, int c);
 
+/*Function that convert day*/
+int convert_day(int month, int day);
+
+/*Function that print remaining*/
+void print_remaining_days(int month, int day, int year);
+
 #endif /* HOLBERTON_H */
