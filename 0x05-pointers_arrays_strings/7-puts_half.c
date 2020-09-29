bf2d0ba6e8c0f882fe663c9a875eac771d0b9f5b
el-dani-cortes <2304@holbertonschool.com>
2020-09-29 18:30:19 +0000

    Add if condition

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,25 @@
  */
 void puts_half(char *str)
 {
-	int len, i;
-	char ch, *count;
+	int len = 0, i;
 
-	len = 0;
-	count = str;
-	while (*count != '\0')
+	while (str[len] != '\0')
 	{
-		count++;
 		len++;
 	}
-	for (i = (len - 1) / 2 ; i < len ; i++)
+	if (len % 2 == 0)
 	{
-		ch = *(str + i);
-		_putchar(ch);
+		for (i = len / 2 ; i < len ; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	else
+	{
+		for (i = (len - 1) / 2 ; i < len ; i++)
+		{
+			_putchar (*(str + i));
+		}
 	}
 	_putchar(10);
 }
