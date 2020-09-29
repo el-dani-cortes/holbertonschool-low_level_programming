e606cad52b10be3a351caf39967ff64eb6136e2a
el-dani-cortes <2304@holbertonschool.com>
2020-09-29 17:28:36 +0000

    Add 7-puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Prints a string.
+ * @str: Variable string.
+ *
+ * Return: Always void.
+ */
+void puts_half(char *str)
+{
+	int len, i;
+	char ch, *count;
+
+	len = 0;
+	count = str;
+	while (*count != '\0')
+	{
+		count++;
+		len++;
+	}
+	for (i = len / 2 ; i < len ; i++)
+	{
+		ch = *(str + i);
+		_putchar(ch);
+	}
+	_putchar(10);
+}
