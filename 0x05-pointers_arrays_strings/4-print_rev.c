d237e1becdee14cd23a5a444bc3fe98c182013c5
el-dani-cortes <2304@holbertonschool.com>
2020-09-29 05:08:46 +0000

    Add all files until 4 task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+B#include "holberton.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: Variable string.
+ *
+ * Return: Always void.
+ */
+void print_rev(char *s)
+{
+	char *p;
+
+	p = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	for (; s >= p ; --s)
+	{
+		_putchar(*s);
+	}
+	_putchar(10);
+}
