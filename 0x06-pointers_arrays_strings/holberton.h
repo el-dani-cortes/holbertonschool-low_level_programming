9a2d8be0357c9b2f338344ba9c6fd8c1f6a73063
el-dani-cortes <2304@holbertonschool.com>
2020-10-01 19:51:49 +0000

    Added 4-task and updated header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -16,4 +16,7 @@ char *_strncpy(char *dest, char *src, int n);
 /*Function that compares two strings*/
 int _strcmp(char *s1, char *s2);
 
+/*Function that reverses the content of an array of integers*/
+void reverse_array(int *a, int n);
+
 #endif /* HOLBERTON_H */
