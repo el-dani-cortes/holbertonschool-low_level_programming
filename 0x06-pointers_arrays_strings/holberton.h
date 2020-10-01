a21e8ece8824f48f63c65ee231664cd8cd1ea45e
el-dani-cortes <2304@holbertonschool.com>
2020-10-01 17:49:23 +0000

    Adde files to 2 - task and upload the header

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function that prints characters*/
+int _putchar(char c);
+
+/*Function that concatenates two strings*/
+char *_strcat(char *dest, char *src);
+
+/*Function concatenates two strings, but have to use at most n bytes from src*/
+char *_strncat(char *dest, char *src, int n);
+
+/*Function that copies a string*/
+char *_strncpy(char *dest, char *src, int n);
+
+#endif /* HOLBERTON_H */
