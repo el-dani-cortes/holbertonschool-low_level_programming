4aeca8ec2634e43bc2b4a2846de2413973e8461a
el-dani-cortes <2304@holbertonschool.com>
2020-10-05 18:35:28 +0000

    Added 0-task,  1-task and header

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function that prints characters*/
+int _putchar(char c);
+
+/*Function that fills memory with a constant byte*/
+char *_memset(char *s, char b, unsigned int n);
+
+/*function that copies memory area*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/*function that locates a character in a string*/
+char *_strchr(char *s, char c);
+
+#endif /* HOLBERTON_H */
