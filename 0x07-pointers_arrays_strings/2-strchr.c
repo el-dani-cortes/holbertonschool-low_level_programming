f4907d8bde29439ac54e7ffcecc19d8ac7a5fd0f
el-dani-cortes <2304@holbertonschool.com>
2020-10-05 19:21:54 +0000

    Added 2-task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Function that locates a character in a string.
+ * @s: Pointer to a string.
+ * @c: Character to locate in the string.
+ *
+ * Return: Pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return(s);
+		}
+		s++;
+	}
+	return(s+1);
+}
