e95c89b91eac13e7d5ff1f9ee578d26d674c4054
el-dani-cortes <2304@holbertonschool.com>
2020-10-05 19:42:22 +0000

    Change to a for loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	for (; ; s++)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		s++;
+		else if (*s == '\0')
+		{
+			return (s);
+		}
 	}
-	return ('\0');
 }
