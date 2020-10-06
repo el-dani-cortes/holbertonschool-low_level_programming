57d86f627db6e71c9bbc232c8d91788084818d18
el-dani-cortes <2304@holbertonschool.com>
2020-10-06 00:23:40 +0000

    Changed loop

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,20 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 	unsigned int count = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0 ; s[j] != '\0'; j++)
+		for (j = 0 ; accept[j] != '\0'; j++)
 		{
-			if (s[j] == accept[i])
+			if (s[i] == accept[j])
 			{
-			       	count++;
-				break;		
+				count++;
+				break;
 			}
 		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
 	}
-	return(count+1);
+	return (count);
 }
-			    
