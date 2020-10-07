2f59cf5a8808d464e8f5e4a3bcbd35969f92a2ac
el-dani-cortes <2304@holbertonschool.com>
2020-10-07 17:30:29 +0000

    Added 0, 1 task and header

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function to print a character*/
+int _putchar(char c);
+
+/*Function that prints a string*/
+void _puts_recursion(char *s);
+
+/*Function to print a string in reverse*/
+void _print_rev_recursion(char *s);
+
+/*Function that returns the length of a string*/
+int _strlen_recursion(char *s);
+
+#endif /* HOLBERTON_H */
