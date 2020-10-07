160b0b07ec40ef667a4642ce0ad284d86b2ff1a8
el-dani-cortes <2304@holbertonschool.com>
2020-10-07 19:14:51 +0000

    Added 3-task and updated header

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -13,4 +13,7 @@ void _print_rev_recursion(char *s);
 /*Function that returns the length of a string*/
 int _strlen_recursion(char *s);
 
+/*Function that return factorial number*/
+int factorial(int n);
+
 #endif /* HOLBERTON_H */
