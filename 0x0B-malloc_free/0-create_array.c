517ae260716dec9c138143067cefbe9fcbe00dc6
el-dani-cortes <2304@holbertonschool.com>
2020-10-13 17:15:03 +0000

    Added a condition in the first if

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
-	if (size == 0)
+	ptr = malloc(sizeof(char) * size);
+	if (size == 0 || ptr == NULL)
 	{
 		return ('\0');
 	}
 	else
 	{
-		ptr = malloc(sizeof(char) * size);
 		for (i = 0; i < size; i++)
 		{
 			ptr[i] = c;
