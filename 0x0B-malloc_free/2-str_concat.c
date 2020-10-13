75fca33e2f7139431221d83747ca372a322651ab
el-dani-cortes <2304@holbertonschool.com>
2020-10-13 19:57:46 +0000

    Added 2-task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,21 +22,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = ("");
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 	len1 = i + 1;
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
 	len2 = j + 1;
-	ptr = malloc(sizeof(char) * (len1 + len2));
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	else
+	ptr = malloc(sizeof(char) * (len1 + len2 - 1));
+	if (ptr != NULL)
 	{
 		for (k = 0; k <= (len1 - 1); k++)
 		{
@@ -47,5 +39,7 @@ char *str_concat(char *s1, char *s2)
 			ptr[(len1 - 1) + k] = s2[k];
 		}
 	}
+	else
+		return ('\0');
 	return (ptr);
 }
