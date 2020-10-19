9f0078060ba8a810c9210ce2792b8028eca77f73
el-dani-cortes <2304@holbertonschool.com>
2020-10-19 18:20:58 +0000

    Added header with a new type struct dog and readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef __FILE__H_
+#define __FILE__H_
+
+/*Function to print characters*/
+int _putchar(char c);
+
+/**
+ * struct dog - Variables for dog description
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's name
+ *
+ * Description: Variables to define in detail a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* FILE_H */
