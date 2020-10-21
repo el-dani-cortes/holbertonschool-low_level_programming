8cb1a8811324c63f963284ee60871dd313261bb8
el-dani-cortes <2304@holbertonschool.com>
2020-10-21 18:58:21 +0000

    Changed the order of the if condition. Put first the if condition for size

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i, j = 0;
 	int compare_value;
 
-	if (array != NULL && size != 0 && cmp != NULL)
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
 	{
-		if (size <= 0)
-			return (-1);
 		for (i = 0; i < size; i++)
 		{
 			compare_value = (*cmp)(array[i]);
