15196a8e1d59147e66908397237a16c3c5c3ddf8
el-dani-cortes <2304@holbertonschool.com>
2020-10-24 18:30:16 +0000

    Deleted a if condition and put the new line at the end of the code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,13 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num_list, int));
-		if (i == n - 1)
-		{
-			printf("\n");
-			break;
-		}
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(num_list);
 }
