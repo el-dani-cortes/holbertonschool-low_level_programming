b2255d7341065d3e651b8db492833b743b8ba232
el-dani-cortes <2304@holbertonschool.com>
2020-10-24 17:57:50 +0000

    Added a function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to separate numbers
+ * @n: numbers of arguments
+ *
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num_list;
+	unsigned int i;
+
+	va_start(num_list, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (va_arg(num_list,int != NULL))
+			{
+				printf("%d", va_arg(num_list, int));
+				if (i == n - 1)
+				{
+					printf("\n");
+					break;
+				}
+				printf("%s", separator);
+			}
+			else
+				printf("(nil)");
+
+			va_end(num_list);
+		else
+			printf("(nil)")
+	}
+	printf
+}
