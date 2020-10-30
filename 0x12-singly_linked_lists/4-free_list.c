b2403ec3d154fc88579d83837a2e5725b269a7df
el-dani-cortes <2304@holbertonschool.com>
2020-10-30 15:25:15 +0000

    Added a if condition asking for null head

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,14 @@ void free_list(list_t *head)
 {
 	list_t *tmp_node;
 
-	while (head != NULL)
+	if (head != NULL)
 	{
-		tmp_node = head;
-		head = head->next;
-		free(tmp_node->str);
-		free(tmp_node);
+		while (head != NULL)
+		{
+			tmp_node = head;
+			head = head->next;
+			free(tmp_node->str);
+			free(tmp_node);
+		}
 	}
 }
