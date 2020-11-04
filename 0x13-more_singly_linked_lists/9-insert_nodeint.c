3f9677eb13d889c7a6c12ae842b02a259c21ec14
el-dani-cortes <2304@holbertonschool.com>
2020-11-04 10:01:39 +0000

    Added a if condition when idx is equal to position zero and asign insert the new node in the position zero

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	tmp_node = *head;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (i < idx)
 	{
 		tmp_node = tmp_node->next;
