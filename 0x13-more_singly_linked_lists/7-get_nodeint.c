843ec4a490b9bcabf8f33e817a6ad66bb2a6782f
el-dani-cortes <2304@holbertonschool.com>
2020-11-04 09:41:37 +0000

    Added a condition if head exist when the index is found

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			head = head->next;
 			i++;
 			if (i == index)
-				return (head);
+			{
+				if (head)
+					return (head);
+				else
+					return (NULL);
+			}
 		}
 	}
 	return (NULL);
