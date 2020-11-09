be363c5fa8c630106479c673f80b5f9f63e44f67
el-dani-cortes <2304@holbertonschool.com>
2020-11-09 17:49:51 +0000

    Put the validation of text content at the beginning of the code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
-	for (i = 0; text_content[i]; i++)
-	{}
 	if (text_content == NULL)
 		text_content = "";
+	for (i = 0; text_content[i]; i++)
+	{}
 	if (filename)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
