1e6ed234b4680e9e42c1d737613302f1034bfef9
el-dani-cortes <2304@holbertonschool.com>
2020-11-09 17:56:15 +0000

    Added a condition if to validate text_content is not NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,8 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i, validate;
 
+	if (text_content == NULL)
+		text_content = "";
 	for (i = 0; text_content[i]; i++)
 	{}
 	if (filename)
