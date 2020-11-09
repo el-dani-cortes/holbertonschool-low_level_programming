bf803b4cb32e7b10d8f6b252e4bd70a0f888d17b
el-dani-cortes <2304@holbertonschool.com>
2020-11-09 17:33:29 +0000

    Changed the numbers of bytes to write. Put i-1 to eliminate the null character

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		close(fd);
-		validate = write(fd, text_content, i);
+		validate = write(fd, text_content, i - 1);
 		if (validate == -1)
 			return (-1);
+		close(fd);
 		return (1);
 	}
 	return (-1);
