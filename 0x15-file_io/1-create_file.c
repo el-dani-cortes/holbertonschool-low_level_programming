841ae9fb6af2fbb410819541423fe18d23d47522
el-dani-cortes <2304@holbertonschool.com>
2020-11-09 17:30:52 +0000

    Changed the position the close function, put it before the write function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
+		close(fd);
 		validate = write(fd, text_content, i);
 		if (validate == -1)
 			return (-1);
-		close(fd);
 		return (1);
 	}
 	return (-1);
