ade25b0d374dbf82f6aaba2b1e6d0cb56af6cbb8
el-dani-cortes <2304@holbertonschool.com>
2020-11-10 02:39:03 +0000

    Changed the way it call error function for open, write and read

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,45 +2,46 @@
 
 /**
  * error_close - function that prints the error when close fail.
- * @close_from: validate number for the close function of the original file.
- * @close_to: validate number for the close function of the file created.
- * @fd_from: File descriptor for the open function of the original file.
- * @fd_to: File descriptor for the open function of file created.
+ * @close_file: validate number for the close function.
+ * @fd: File descriptor for the open function of the open function.
  *
  * Return: Always nothing.
  */
-void error_close(int close_from, int close_to, int fd_from, int fd_to)
+void error_close(int close_file, int fd)
 {
-	if (close_from == -1)
+	if (close_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 /**
  * error_open - function that prints the error when open fail.
- * @fd_from: File descriptor for the open function of the original file.
- * @fd_to: File descriptor for the open function of file created.
- * @f_from: Name of the file to copy.
- * @f_to: Name of the file to copy the content of other file.
+ * @fd: File descriptor for the open function of the original file.
+ * @file_name: Name of the file to copy.
  *
  * Return: Always nothing.
  */
-void error_open(int fd_from, int fd_to, const char *f_from, const char *f_to)
+void error_open(int fd, const char *file_name)
 {
-	if (fd_from == -1)
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
 		exit(98);
 	}
-	if (fd_to == -1)
+}
+/**
+ * error_write - function that prints the error when open fail.
+ * @fd: File descriptor for the open function of file created.
+ * @file_name: Name of the file to copy the content of other file.
+ *
+ * Return: Always nothing.
+ */
+void error_write(int fd, const char *file_name)
+{
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", f_to);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_name);
 		exit(99);
 	}
 }
@@ -60,8 +61,9 @@ int copy_file(const char *file_from, const char *file_to)
 	if (!buf)
 		return (-1);
 	fd_from = open(file_from, O_RDONLY);
+	error_open(fd_from, file_from);
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	error_open(fd_from, fd_to, file_from, file_to);
+	error_write(fd_to, file_to);
 	while ((num_bytes = read(fd_from, buf, 1024)) != 0)
 	{
 		if (num_bytes == -1)
@@ -87,8 +89,9 @@ int copy_file(const char *file_from, const char *file_to)
 	}
 	free(buf);
 	close_from = close(fd_from);
+	error_close(close_from, fd_from);
 	close_to = close(fd_to);
-	error_close(close_to, close_from, fd_from, fd_to);
+	error_close(close_to, fd_to);
 	return (0);
 }
 /**
