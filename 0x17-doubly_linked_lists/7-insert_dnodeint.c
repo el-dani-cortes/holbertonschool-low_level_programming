498810260b0237cd730a67690130d563e9d1e7cd
el-dani-cortes <2304@holbertonschool.com>
2020-12-13 18:08:44 +0000

    Added a if condition if head exists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,14 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *behind = NULL, *advance = NULL, *new_node = NULL;
 	unsigned int position = 1;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	advance = (*h)->next;
-	behind = *h;
-	if (*h)
+	if (*h && h)
 	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+			return (NULL);
+		new_node->n = n;
+		advance = (*h)->next;
+		behind = *h;
 		if (idx == 0)
 		{
 			new_node->prev = NULL;
