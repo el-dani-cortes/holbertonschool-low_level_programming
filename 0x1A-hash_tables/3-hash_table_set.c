3b889382881c5173d418e6f4d1090a94f2bc05aa
el-dani-cortes <2304@holbertonschool.com>
2021-01-15 11:25:10 +0000

    Changed the if condition to an or to validate

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t *tmp =  NULL;
 
-	if (ht == NULL && key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
