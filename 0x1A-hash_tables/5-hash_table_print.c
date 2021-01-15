5273aac2a68bc9b8dfc7c813d9c841c4ce8c082b
el-dani-cortes <2304@holbertonschool.com>
2021-01-15 10:27:10 +0000

    Added a fucntion to print the all keys and values of a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function to print all values of the hash table.
+ * @ht: Pointer to the hash table created.
+ *
+ * Return: Always nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int size = 0;
+	hash_node_t *tmp = NULL;
+	unsigned long int i, sw;
+
+	size =  ht->size;
+	printf("{");
+	for (i = 0; i <= size; i++)
+	{
+		tmp = ht->array[i];
+		if (tmp == NULL)
+			continue;
+		if (sw == 1)
+			printf(", ");
+		sw  = 1;
+		while (tmp)
+		{
+			if (tmp->next == NULL)
+			{
+				printf("'%s': '%s'", tmp->key, tmp->value);
+
+			}
+			else
+				printf("'%s': '%s', ", tmp->key, tmp->value);
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
