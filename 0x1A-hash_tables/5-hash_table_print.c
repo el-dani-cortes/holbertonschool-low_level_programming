14bb8dcaa09f02018582d0e45cb0f6e1369791da
el-dani-cortes <2304@holbertonschool.com>
2021-01-15 12:10:27 +0000

    Changed the validation if condition. First validate if ht is NULL

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,27 +13,25 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0, sw = 0;
 
 	size =  ht->size;
-
-	if (ht !=  NULL)
+	if (ht ==  NULL)
+		return;
+	printf("{");
+	for (i = 0; i < size; i++)
 	{
-		printf("{");
-		for (i = 0; i < size; i++)
+		tmp = ht->array[i];
+		if (tmp == NULL)
+			continue;
+		if (sw == 1)
+			printf(", ");
+		sw  = 1;
+		while (tmp)
 		{
-			tmp = ht->array[i];
-			if (tmp == NULL)
-				continue;
-			if (sw == 1)
-				printf(", ");
-			sw  = 1;
-			while (tmp)
-			{
-				if (tmp->next == NULL)
-					printf("'%s': '%s'", tmp->key, tmp->value);
-				else
-					printf("'%s': '%s', ", tmp->key, tmp->value);
-				tmp = tmp->next;
-			}
+			if (tmp->next == NULL)
+				printf("'%s': '%s'", tmp->key, tmp->value);
+			else
+				printf("'%s': '%s', ", tmp->key, tmp->value);
+			tmp = tmp->next;
 		}
-		printf("}\n");
 	}
+	printf("}\n");
 }
